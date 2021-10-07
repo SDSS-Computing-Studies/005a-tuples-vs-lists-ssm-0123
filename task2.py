a5b048ca71ab9bbab6ca676cf48ef0aaf11ec144
ssm-0123 <ssm-0123@users.noreply.github.com>
2021-10-07 14:59:24 -0700

    gogo

--- task2.py
+++ task2.py
@@ -28,7 +28,7 @@ forloop = 0
 words = []
 while loop :
     a = input("Enter a word")
-    words.append(a.strip)
+    words.append(a.strip())
     forloop = forloop + 1
     if forloop == 5:
         loop = False
